2228e14dcd0b2b936c2143f4218c0a0ca7c6f2b4
Michael Pabst <techops+mpabst@change.org>
2012-05-05 22:29:41 -0700

    projects/pruner.rb: dead code trimmer; additions to test/expressions.rubyish

---
+++ projects/pruner.rb
@@ -0,0 +1,64 @@
+# dead code spotter
+
+TYPES = %w( gvars cvars ivars idents consts )
+
+type_method_definer = -> {
+  TYPES.each do |t|
+    define_method(t) do 
+      instance_variable_set("@#{t}") = {} unless instance_variable_get("@#{t}")
+    end
+  end  
+}
+
+class Defined
+  class << self
+    type_method_definer.call
+  end
+end
+
+class Used
+  class << self
+    type_method_definer.call
+  end
+end
+
+ARGV.each do |path|
+  Dir[path].each do |file|
+    xml = Ylyn.new(File.read(file))
+
+    # methods
+    ['def > _ident']
+    # aliasing?
+    # send?
+    # define_method?
+
+    # symbols
+    ['module > _const', 'class > _const', 'module > const_path_ref > _const', 'class > const_path_ref > _const']
+    # symbols on LHS of assignment
+    # constantize?
+
+    # gvars
+
+    # cvars
+    # class_variable_set
+
+    # ivars
+    # instance_variable_set
+
+    # misc idents? - don't bother for now
+
+
+  # capture at moment of definition, look for use elsewhere
+
+  # vvv diminishing returns
+  # _ident inside vs. not inside a vcall - only totes useful if params are required
+  # call vs fcall vs vcall ?
+
+  # unreachable:
+  # compile an index of idents etc used in each code block:
+  # - method bodies
+  # - module-level code
+  # - global-level code
+  # start with controller actions and resque work methods and start marking identifiers via breadth-first
+  # unmarked symbols, methods, and idents are probs dead
+end
